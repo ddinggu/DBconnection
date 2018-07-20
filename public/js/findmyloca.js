c1a59e7687487e8d5592f10f6a6e5b5b9b99fe13
ddinggu <travelrudwls@gmail.com>
2018-07-20 21:25:54 +0900

    사용자 위치 불러오기

--- public/js/findmyloca.js
+++ public/js/findmyloca.js
@@ -1,3 +1,5 @@
+// 기본 위치인 서울시청으로 먼저 맵을 뿌려준다
+// 만약 기본 맵이 뿌려지지 않는다면, scope로 인해 Map object를 이용한 모든 method를 사용할 수 없다. 
 var defaultLoaction = new naver.maps.LatLng(37.5666805, 126.9784147);
 
 var mapOption = {
@@ -13,48 +15,67 @@ var mapOption = {
 
 var map = new naver.maps.Map('map', mapOption);
 
+var marker = new naver.maps.Marker({
+    position : defaultLoaction,
+    map : map
+});
+
+ // 마커 옮기는 리스너 세팅 (marker와 map을 scope때문에 받아 올 수 없다..)
+naver.maps.Event.addListener(map, 'click', function(e) {
+     marker.setPosition(e.coord);
+ });
+
+//------------------- 초기 기본 맵 설정 끝 -----------------------------
+
 console.log(map.center);
 
+//----------------- 위치정보 처리 변수 --------------------------------
+var userLocateAcceptSuccess = function success(position){
+    // 좌표 불러오기
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    // 현 위치를 기준으로 맵 설정 + 중심좌표 변경 
+    map.setCenter(new naver.maps.LatLng(latitude, longitude));
+    map.morph(new naver.maps.LatLng(latitude, longitude), 12);
+    console.log(map.center);
+
+    // 현 위치의 마커 찍기( 마커가 따라 이동할 수 있는지의 여부는 ??)
+    var mylocation = new naver.maps.Marker({
+        position: new naver.maps.LatLng(latitude, longitude), 
+        map: map,
+        icon: {
+            url: 'img/here.png',
+            size: new naver.maps.Size(22, 22),
+            origin: new naver.maps.Point(0, 0),
+            anchor: new naver.maps.Point(25, 26),
+            scaledSize : {width: 22, height: 22} // 이미지의 크기를 조절
+             }
+    });
+}
+
+var userLocateAcceptFailed = function error(){
+    alert('더 나은 사용을 위해 GPS를 켜주시면 좋습니다.');
+}
+
+var userLocateOption = {
+    enableHighAccuracy: false,
+    maximumAge: 0,
+    timeout: Infinity
+};
+
+// load 될 때, 사용자 위치 정보 허용 여부 및 위치 정보를 통한 위치 이동 
+// navigator의 parameter에 따라 성공시 이동, 실패시 서울시청에 가만히 있는다. 
 $(window).on('load', () => {
     if (!navigator.geolocation){
         return;
     }
-    else{
-        navigator.geolocation.getCurrentPosition(
-            function success(position){
-                // 좌표 불러오기
-                var latitude  = position.coords.latitude;
-                var longitude = position.coords.longitude;
-
-                // 현 위치를 기준으로 맵 설정 + 중심좌표 변경 
-                map.setCenter(new naver.maps.LatLng(latitude, longitude));
-                map.morph(new naver.maps.LatLng(latitude, longitude), 12);
-                console.log(map.center);
-
-                // 마커 찍기
-                var marker = new naver.maps.Marker({
-                    position: new naver.maps.LatLng(latitude, longitude), 
-                    map: map
-                });
-       
-                // 마커 옮기는 리스너 세팅 (marker와 map을 scope때문에 받아 올 수 없다..)
-                naver.maps.Event.addListener(map, 'click', function(e) {
-                    marker.setPosition(e.coord);
-                });
-
-            },
-            function error(){
-                var map = new naver.maps.Map('map', mapOption ); 
-            },
-            {
-                enableHighAccuracy: false,
-                maximumAge: 0,
-                timeout: Infinity
-            }
-      ) 
-    }
+    else navigator.geolocation.getCurrentPosition(userLocateAcceptSuccess, userLocateAcceptFailed, userLocateOption) 
 });
 
+//----------------- 위치정보 처리 끝 --------------------------------------
+
+//---------------------- 내 좌표 확인 ------------------------------
 $('#clicklocate').on('click', geoFindMe);
 
 function geoFindMe() {
@@ -81,3 +102,4 @@ function geoFindMe() {
   
     navigator.geolocation.getCurrentPosition(success, error);
   }
+//---------------------- 내 좌표 확인 끝 ------------------------------
--- public/js/naverelements.js
+++ public/js/naverelements.js
@@ -1,6 +1,3 @@
-// 클릭시 마커를 옮기는 역활 
-var markers = []; 
-
 // 이 view를 실행 시 maps에서 설정한 mongodb와 연동한 값들을 data로 불러온다.
 // 이 값들은 $.get(..)에서 data parameter로 설정되고, toArray한 data들을 
 $(window).on('load', () =>{
@@ -28,15 +25,18 @@ function createMaker(location){
          position: new naver.maps.LatLng(location[1], location[0]),
          map: map,
          icon: {
-               url: 'img/sally.png',
-               size: new naver.maps.Size(30, 32),
-               origin: new naver.maps.Point(0, 0),
-               anchor: new naver.maps.Point(25, 26)
-                }
+            url: 'img/sally.png',
+            size: new naver.maps.Size(30, 32),
+            origin: new naver.maps.Point(0, 0),
+            anchor: new naver.maps.Point(25, 26)
+             }
     });
 
     markers.push(marker);
 }
+
+// 동적 마커 표시 역활( 클릭시 마커를 움직이게 하기 위한 필요 도구 )
+var markers = []; 
 
 // idle은 지도의 움직임이 종료 되었을때, 이벤트가 발생하게 된다. 
 naver.maps.Event.addListener(map, 'idle', function() {
@@ -74,19 +74,19 @@ function hideMarker(map, marker) {
 }
 
 
-// // customControler 
-// var locationBtnHtml = '<div class="map_btn_top"></div>';
+// customControler 제작
+var locationBtnHtml = '<a href="#" class="btn_mylct"><span class="spr_trff spr_ico_mylct">GPS</span></a>';
 
-// //customControl 객체를 이용하기
-// var customControl = new naver.maps.CustomControl(locationBtnHtml, {
-//     position: naver.maps.Position.TOP_LEFT
-// });
+//customControl 객체를 이용하여 gps 활성화 및 위치 이동
+var customControl = new naver.maps.CustomControl(locationBtnHtml, {
+    position: naver.maps.Position.TOP_LEFT
+});
 
-// customControl.setMap(map);
+customControl.setMap(map);
 
-// var domEventListener = naver.maps.Event.addDOMListener(customControl.getElement(), 'click', function() {
-//     map.setCenter(new naver.maps.LatLng(37.3595953, 127.1053971));
-// });
+var domEventListener = naver.maps.Event.addDOMListener(customControl.getElement(), 'click', function() {
+    navigator.geolocation.getCurrentPosition(userLocateAcceptSuccess, userLocateAcceptFailed, userLocateOption);
+});
 
 
 // $('.place1').on('click', () => {
